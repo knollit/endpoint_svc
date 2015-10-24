967367795a1a75b5fd66142a9f4039204911301f
Michael Raimondi <mike@mikeraimondi.com>
2015-10-23 23:39:27 -0400

    First pass at integration test

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,10 @@
 BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE endpoints (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc()
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+  watchpointURL text
 );
 COMMENT ON TABLE endpoints IS 'API endpoints registered with the service';
 COMMENT ON COLUMN endpoints.id IS 'UUID of endpoint';
+COMMENT ON COLUMN endpoints.watchpointURL IS 'URL of endpoint schema to monitor';
 COMMIT;
--- main_test.go
+++ main_test.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"net"
 	"testing"
+	"time"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/mikeraimondi/knollit/common"
+	endpointProto "github.com/mikeraimondi/knollit/endpoints/proto"
 )
 
 type logWriter struct {
@@ -19,10 +22,19 @@ func (l *logWriter) Write(p []byte) (n int, err error) {
 }
 
 func TestServer(t *testing.T) {
-	// TODO setup DB
-
 	db, _ := sql.Open("postgres", "user=mike host=localhost dbname=endpoints_test sslmode=disable")
 
+	// Test setup
+	if err := db.Ping(); err != nil {
+		t.Fatal(err)
+	}
+	db.Exec("BEGIN")
+	defer db.Exec("ROLLBACK")
+
+	const watchpointURL = "test"
+	db.Exec("INSERT INTO endpoints (watchpointURL) VALUES ($1)", watchpointURL)
+
+	// Server setup
 	const addr = ":13900"
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -33,24 +45,47 @@ func TestServer(t *testing.T) {
 		db:       db,
 		listener: l,
 		ready:    rdy,
-		logger:   log.New(&logWriter{t}, "", log.LstdFlags),
+		logger:   log.New(&logWriter{t}, "", log.Lmicroseconds),
 	}
 
+	// Server startup
 	errs := make(chan error)
-
 	go func() {
 		errs <- server.run()
 	}()
 	select {
 	case err = <-errs:
 		t.Error(err)
+	case <-time.NewTimer(10 * time.Second).C:
+		t.Fatal("Timed out waiting for server to start")
 	case <-rdy:
+
+		// Begin test
 		conn, err := net.Dial("tcp", addr)
 		if err != nil {
 			t.Fatal(err)
 		}
 		defer conn.Close()
 
-		common.WriteWithSize(conn, []byte("foo"))
+		data, err := proto.Marshal(&endpointProto.Request{
+			Action: endpointProto.Request_INDEX,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		common.WriteWithSize(conn, data)
+
+		buf, _, err := common.ReadWithSize(conn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		endpointMsg := &endpointProto.Endpoint{}
+		if err := proto.Unmarshal(buf, endpointMsg); err != nil {
+			t.Fatal(err)
+		}
+
+		if endpointMsg.WatchpointURL != watchpointURL {
+			t.Fatalf("Expected %v for watchpointURL, got %v", watchpointURL, endpointMsg.WatchpointURL)
+		}
 	}
 }
