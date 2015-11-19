c8baad61957e1d3572339950e1668848c26223bf
Michael Raimondi <mike@mikeraimondi.com>
2015-11-18 22:16:56 -0500

    DRY up integration test setup

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 	"io/ioutil"
 	"log"
 	"net"
@@ -50,159 +49,137 @@ func setupDB() (db *sql.DB, err error) {
 	return
 }
 
-func runServer(s *server, rdy chan int) error {
+func runWithServer(t *testing.T, testFunc func(*server)) {
+	// Setup DB
+	db, err := setupDB()
+	if err != nil {
+		t.Fatal("Error setting up DB: ", err)
+	}
+	defer db.Exec("ROLLBACK")
+
+	// Setup server
+	const addr = ":13900" // TODO pick a better number?
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rdy := make(chan int)
+	s := &server{
+		db:       db,
+		listener: l,
+		ready:    rdy,
+		logger:   log.New(&logWriter{t}, "", log.Lmicroseconds),
+	}
+	defer s.Close()
+
+	// Start server on another goroutine
 	errs := make(chan error)
 	go func() {
 		errs <- s.run()
 	}()
 	select {
 	case err := <-errs:
-		return err
+		t.Fatal(err)
 	case <-time.NewTimer(10 * time.Second).C:
-		return errors.New("Timed out waiting for server to start")
+		t.Fatal("Timed out waiting for server to start")
 	case <-rdy:
-		return nil
+		testFunc(s)
 	}
 }
 
 func TestEndpointIndexWithOne(t *testing.T) {
-	db, err := setupDB()
-	if err != nil {
-		t.Fatal("Error setting up DB: ", err)
-	}
-	defer db.Exec("ROLLBACK")
+	runWithServer(t, func(s *server) {
+		// Test-specific setup
+		const URL = "test"
+		const org = "testOrg"
+		if _, err := s.db.Exec("INSERT INTO endpoints (URL, organization) VALUES ($1, $2)", URL, org); err != nil {
+			t.Fatal(err)
+		}
 
-	// Test-specific setup
-	const URL = "test"
-	const org = "testOrg"
-	if _, err := db.Exec("INSERT INTO endpoints (URL, organization) VALUES ($1, $2)", URL, org); err != nil {
-		t.Fatal(err)
-	}
+		// Begin test
+		conn, err := net.Dial("tcp", s.listener.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer conn.Close()
 
-	// Server setup
-	const addr = ":13900"
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rdy := make(chan int)
-	server := &server{
-		db:       db,
-		listener: l,
-		ready:    rdy,
-		logger:   log.New(&logWriter{t}, "", log.Lmicroseconds),
-	}
+		b := flatbuffers.NewBuilder(0)
+		endpointReq := endpoint{
+			Action: endpoints.ActionIndex,
+		}
+		if _, err := common.WriteWithSize(conn, endpointReq.toFlatBufferBytes(b)); err != nil {
+			t.Fatal(err)
+		}
 
-	if err := runServer(server, rdy); err != nil {
-		t.Fatal(err)
-	}
-	defer server.Close()
+		buf, _, err := common.ReadWithSize(conn)
+		if err != nil {
+			t.Fatalf("Error reading response from server: %v", err)
+		}
+		endpointMsg := endpoints.GetRootAsEndpoint(buf, 0)
 
-	// Begin test
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
-
-	b := flatbuffers.NewBuilder(0)
-	endpointReq := endpoint{
-		Action: endpoints.ActionIndex,
-	}
-	if _, err := common.WriteWithSize(conn, endpointReq.toFlatBufferBytes(b)); err != nil {
-		t.Fatal(err)
-	}
-
-	buf, _, err := common.ReadWithSize(conn)
-	if err != nil {
-		t.Fatalf("Error reading response from server: %v", err)
-	}
-	endpointMsg := endpoints.GetRootAsEndpoint(buf, 0)
-
-	if len(string(endpointMsg.Id())) <= 24 {
-		t.Fatalf("Expected UUID for ID, got %v", string(endpointMsg.Id()))
-	}
-	if string(endpointMsg.URL()) != URL {
-		t.Fatalf("Expected %v for URL, got %v", URL, endpointMsg.URL)
-	}
-	if string(endpointMsg.Organization()) != org {
-		t.Fatalf("Expected %v for organization, got %v", org, endpointMsg.Organization)
-	}
+		if len(string(endpointMsg.Id())) <= 24 {
+			t.Fatalf("Expected UUID for ID, got %v", string(endpointMsg.Id()))
+		}
+		if string(endpointMsg.URL()) != URL {
+			t.Fatalf("Expected %v for URL, got %v", URL, endpointMsg.URL)
+		}
+		if string(endpointMsg.Organization()) != org {
+			t.Fatalf("Expected %v for organization, got %v", org, endpointMsg.Organization)
+		}
+	})
 }
 
 func TestEndpointReadWithTwo(t *testing.T) {
-	db, err := setupDB()
-	if err != nil {
-		t.Fatal("Error setting up DB: ", err)
-	}
-	defer db.Exec("ROLLBACK")
+	runWithServer(t, func(s *server) {
 
-	// Test-specific setup
-	const id2 = "5ff0fcbd-8b51-11e5-a171-df11d9bd7d62"
-	const URL2 = "test2"
-	const org2 = "testOrg2"
-	if _, err := db.Exec("INSERT INTO endpoints (id, url, organization) VALUES ($1, $2, $3)", id2, URL2, org2); err != nil {
-		t.Fatal(err)
-	}
-	const id = "5ff0fcbc-8b51-11e5-a171-df11d9bd7d62"
-	const URL = "test"
-	const org = "testOrg"
-	if _, err := db.Exec("INSERT INTO endpoints (id, url, organization) VALUES ($1, $2, $3)", id, URL, org); err != nil {
-		t.Fatal(err)
-	}
+		// Test-specific setup
+		const id2 = "5ff0fcbd-8b51-11e5-a171-df11d9bd7d62"
+		const URL2 = "test2"
+		const org2 = "testOrg2"
+		if _, err := s.db.Exec("INSERT INTO endpoints (id, url, organization) VALUES ($1, $2, $3)", id2, URL2, org2); err != nil {
+			t.Fatal(err)
+		}
+		const id = "5ff0fcbc-8b51-11e5-a171-df11d9bd7d62"
+		const URL = "test"
+		const org = "testOrg"
+		if _, err := s.db.Exec("INSERT INTO endpoints (id, url, organization) VALUES ($1, $2, $3)", id, URL, org); err != nil {
+			t.Fatal(err)
+		}
 
-	// Server setup
-	const addr = ":13900"
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rdy := make(chan int)
-	server := &server{
-		db:       db,
-		listener: l,
-		ready:    rdy,
-		logger:   log.New(&logWriter{t}, "", log.Lmicroseconds),
-	}
+		// Begin test
+		conn, err := net.Dial("tcp", s.listener.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer conn.Close()
 
-	if err := runServer(server, rdy); err != nil {
-		t.Fatal(err)
-	}
-	defer server.Close()
+		b := flatbuffers.NewBuilder(0)
+		endpointReq := endpoint{
+			ID:           id,
+			Organization: org,
+			Action:       endpoints.ActionRead,
+		}
 
-	// Begin test
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
+		if _, err := common.WriteWithSize(conn, endpointReq.toFlatBufferBytes(b)); err != nil {
+			t.Fatal(err)
+		}
 
-	b := flatbuffers.NewBuilder(0)
-	endpointReq := endpoint{
-		ID:           id,
-		Organization: org,
-		Action:       endpoints.ActionRead,
-	}
+		buf, _, err := common.ReadWithSize(conn)
+		if err != nil {
+			t.Fatalf("Error reading response from server: %v", err)
+		}
+		endpointMsg := endpoints.GetRootAsEndpoint(buf, 0)
 
-	if _, err := common.WriteWithSize(conn, endpointReq.toFlatBufferBytes(b)); err != nil {
-		t.Fatal(err)
-	}
-
-	buf, _, err := common.ReadWithSize(conn)
-	if err != nil {
-		t.Fatalf("Error reading response from server: %v", err)
-	}
-	endpointMsg := endpoints.GetRootAsEndpoint(buf, 0)
-
-	if msgID := string(endpointMsg.Id()); msgID != id {
-		t.Fatalf("Expected %v for ID, got %v", id, msgID)
-	}
-	if msgURL := string(endpointMsg.URL()); msgURL != URL {
-		t.Fatalf("Expected %v for URL, got %v", URL, msgURL)
-	}
-	if msgOrg := string(endpointMsg.Organization()); msgOrg != org {
-		t.Fatalf("Expected %v for organization, got %v", org, msgOrg)
-	}
+		if msgID := string(endpointMsg.Id()); msgID != id {
+			t.Fatalf("Expected %v for ID, got %v", id, msgID)
+		}
+		if msgURL := string(endpointMsg.URL()); msgURL != URL {
+			t.Fatalf("Expected %v for URL, got %v", URL, msgURL)
+		}
+		if msgOrg := string(endpointMsg.Organization()); msgOrg != org {
+			t.Fatalf("Expected %v for organization, got %v", org, msgOrg)
+		}
+	})
 }
 
 func TestAllEndpoints(t *testing.T) {
